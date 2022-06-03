3a0a31abded970c953eecd706bcc951e0517e742
Slava <tfdf63@yandex.ru>
2022-06-03 22:26:27 +0400

    dva file

--- js/script.js
+++ js/script.js
@@ -9,13 +9,15 @@ const personalMovieDB = {
 };
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('Насколько вы оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('Насколько вы оцените его?', '');
+    b = prompt('Насколько вы оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('Насколько вы оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
-//*привет тест
+//привет тест
+
+//Ура ура ура
