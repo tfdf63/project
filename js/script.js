0d66cdfccf2406bfb76eb56e1888ce4ddc3d96f8
Slava <tfdf63@yandex.ru>
2022-07-12 18:18:32 +0400

    js urok massiv

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
 "use srtict"
 
-if (4 == 5) {
-    console.log('ОК');
-} else {
-    console.log('Error');
+const arr = [1, 22, 23, 6, 8];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
 }
 
-let a = 155;
+// arr.pop();
+// arr.push(12);
+// console.log(arr);
 
-if (a < 49) {
-    console.log('Error');
-} else if (a > 100) {
-    console.log('Много');
-} console.log('ОК');
-
-
-switch (a) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 155:
-        console.log('Верно');
-        break;
-    default:
-        console.log('Неверно');
-        break;
+/*
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
 }
+
+for (let value of arr) {
+    console.log(value);
+}
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${1}: ${item} внутри массива ${arr}`);
+});
+*/
+
+// const str = prompt("","");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
